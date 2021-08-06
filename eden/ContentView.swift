b3a0ec7dce40f738930d94450f404d042671ca1d
Christine Yang <yang.christine.95@gmail.com>
2021-08-06 17:54:42 -0400

    Genesis

---
+++ eden/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eden
+//
+//  Created by Christine Yang on 8/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eden/edenApp.swift
@@ -0,0 +1,17 @@
+//
+//  edenApp.swift
+//  eden
+//
+//  Created by Christine Yang on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct edenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
